1697cd3b22dc2e205f141f29811a407e33279fe9
realisshoon <hsgn21@naver.com>
2023-11-28 01:01:43 +0900

    Changes

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,20 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@Controller //첫 진입점
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("hello") //hello 라는 메소드 호출
     public String hello(Model model){
         model.addAttribute("data","hello ~~~!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
