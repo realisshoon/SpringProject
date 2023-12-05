b131e9bb696535b9075b60ee3519d48e5519b1fe
realisshoon <hsgn21@naver.com>
2023-12-06 00:58:54 +0900

    회원서비스 구현, 회원서비스 Testcase(진행중)

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,50 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 x
+
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m->{
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+
+        validateDuplicateMember(member); // mac 기준) control+T 로 메소드 리팩토링
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) { //메소트 리펙토링 진행
+        memberRepository.findByName(member.getName())
+             .ifPresent(m->{
+                 throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+}
